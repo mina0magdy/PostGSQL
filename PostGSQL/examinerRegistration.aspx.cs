15146c8ce6a35c29e1dfbb7efec5ce8ac97e0497
mina0magdy <92601097+mina0magdy@users.noreply.github.com>
2021-12-22 14:22:22 +0200

    examiner registration

--- PostGSQL/examinerRegistration.aspx.cs
+++ PostGSQL/examinerRegistration.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -16,6 +19,40 @@ namespace PostGSQL
 
         protected void Register(object sender, EventArgs e)
         {
+
+            String connStr = WebConfigurationManager.ConnectionStrings["PostGSQL"].ToString();
+
+            SqlConnection conn = new SqlConnection(connStr);
+            String exName = name.Text;
+            String email = Email.Text;
+            String pass = Password.Text;
+            String national = National.SelectedItem.Value;
+            String field = fieldOfWork.Text;
+
+            Boolean nationalBit;
+            if (national == "1")
+                nationalBit = true;
+            else
+                nationalBit = false;
+
+
+
+            SqlCommand ExaminerRegister = new SqlCommand("ExaminerRegister", conn);
+            ExaminerRegister.CommandType = CommandType.StoredProcedure;
+
+
+            ExaminerRegister.Parameters.Add(new SqlParameter("@name", SqlDbType.VarChar)).Value = exName;
+            ExaminerRegister.Parameters.Add(new SqlParameter("@fieldOfWork", SqlDbType.VarChar)).Value = field;
+            ExaminerRegister.Parameters.Add(new SqlParameter("@isNational", SqlDbType.Bit)).Value = nationalBit;
+            ExaminerRegister.Parameters.Add(new SqlParameter("@email", SqlDbType.VarChar)).Value = email;
+            ExaminerRegister.Parameters.Add(new SqlParameter("@password", SqlDbType.VarChar)).Value = pass;
+
+
+            conn.Open();
+            ExaminerRegister.ExecuteNonQuery();
+            conn.Close();
+
+            Response.Redirect("loginPage.aspx");
 
         }
     }
