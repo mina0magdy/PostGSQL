48e1d0a7bfeef1a56d85e73c575c42db18ee0aa4
mina0magdy <92601097+mina0magdy@users.noreply.github.com>
2021-12-22 13:49:08 +0200

    supervisor register

--- PostGSQL/supervisorRegistration.aspx.cs
+++ PostGSQL/supervisorRegistration.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -14,14 +17,37 @@ namespace PostGSQL
 
         }
 
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         protected void Register(object sender, EventArgs e)
         {
+            String connStr = WebConfigurationManager.ConnectionStrings["PostGSQL"].ToString();
 
+            SqlConnection conn = new SqlConnection(connStr);
+
+
+            String firstname = firstName.Text;
+            String lastname = lastName.Text;
+            String mail = email.Text;
+            String pass = password.Text;
+            String ffaculty = faculty.Text;
+            String aaddress = address.Text;
+
+
+            SqlCommand SupervisorRegister = new SqlCommand("SupervisorRegister", conn);
+            SupervisorRegister.CommandType = CommandType.StoredProcedure;
+
+            SupervisorRegister.Parameters.Add(new SqlParameter("@first_name", SqlDbType.VarChar)).Value = firstname;
+            SupervisorRegister.Parameters.Add(new SqlParameter("@last_name", SqlDbType.VarChar)).Value = lastname;
+            SupervisorRegister.Parameters.Add(new SqlParameter("@password", SqlDbType.VarChar)).Value = pass;
+            SupervisorRegister.Parameters.Add(new SqlParameter("@faculty", SqlDbType.VarChar)).Value = ffaculty;
+            SupervisorRegister.Parameters.Add(new SqlParameter("@email", SqlDbType.VarChar)).Value = mail;
+            SupervisorRegister.Parameters.Add(new SqlParameter("@address", SqlDbType.VarChar)).Value = aaddress;
+
+            conn.Open();
+            SupervisorRegister.ExecuteNonQuery();
+            conn.Close();
+
+            Response.Redirect("loginPage.aspx");
         }
     }
 }
